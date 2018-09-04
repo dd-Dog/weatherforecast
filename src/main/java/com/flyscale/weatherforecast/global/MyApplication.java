ad35b339d76fbd8696f0a15dd5c82b9e124963a6
bianjb <bianjb@flyscale.cn>
2018-09-04 13:30:40 +0800

    f66版本去掉跑流量

--- src/main/java/com/flyscale/weatherforecast/global/MyApplication.java
+++ src/main/java/com/flyscale/weatherforecast/global/MyApplication.java
@@ -38,8 +38,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        initProperties();
-        calculateTaskTime();
     }
 
     @SuppressLint("HardwareIds")
